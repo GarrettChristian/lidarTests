53811b45eb0957b04804f96c86d67c2ae9d0b054
GarrettChristian <garrettchristian2@gmail.com>
2022-03-26 15:49:30 -0400

    attempted to add generator

--- autoencoder/3encoderThreeNewNet/anomolyDetector.py
+++ autoencoder/3encoderThreeNewNet/anomolyDetector.py
@@ -1,13 +1,10 @@
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import tensorflow as tf
 
-from sklearn.metrics import accuracy_score, precision_score, recall_score
-from sklearn.model_selection import train_test_split
-from tensorflow.keras import layers, losses
-from tensorflow.keras.datasets import fashion_mnist
+
 from tensorflow.keras.models import Model
+from tensorflow.keras import layers
+from tensorflow import keras
 
 
 class AnomalyDetector(Model):
@@ -28,4 +25,59 @@ class AnomalyDetector(Model):
     decoded = self.decoder(encoded)
     return decoded
 
+# Adapted from this tutorial:
+# https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
+class DataGenerator(keras.utils.Sequence):
+  def __init__(self, list_IDs, labels, batch_size=100, dim=(2070272), n_channels=1, shuffle=True):
+      'Initialization'
+      self.dim = dim
+      self.batch_size = batch_size
+      self.labels = labels
+      self.list_IDs = list_IDs
+      self.n_channels = n_channels
+      self.shuffle = shuffle
+      self.on_epoch_end()
 
+  def on_epoch_end(self):
+    'Updates indexes after each epoch'
+    self.indexes = np.arange(len(self.list_IDs))
+    if self.shuffle == True:
+        np.random.shuffle(self.indexes)
+
+  def __data_generation(self, list_IDs_temp):
+    'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+    # Initialization
+    # X = np.empty((self.batch_size, *self.dim, self.n_channels))
+    X = np.empty((self.batch_size, *self.dim))
+    y = np.empty((self.batch_size), dtype=int)
+
+    # Generate data
+    for i, ID in enumerate(list_IDs_temp):
+        # Store sample
+        # X[i,] = np.load(ID)
+        pcdi = np.fromfile(ID, dtype=np.float32)
+        X[i,] = np.pad(pcdi, (0,2070272 - int(np.shape(pcdi)[0])), 'constant', constant_values=(0))
+
+        # Store class
+        # y[i] = self.labels[ID]
+        y[i] = 1
+
+    # return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
+    return X, y
+
+  def __len__(self):
+    'Denotes the number of batches per epoch'
+    return int(np.floor(len(self.list_IDs) / self.batch_size))
+
+  def __getitem__(self, index):
+    'Generate one batch of data'
+    # Generate indexes of the batch
+    indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+    # Find list of IDs
+    list_IDs_temp = [self.list_IDs[k] for k in indexes]
+
+    # Generate data
+    X, y = self.__data_generation(list_IDs_temp)
+
+    return X, y
--- autoencoder/3encoderThreeNewNet/trainEcgAutoEnc.py
+++ autoencoder/3encoderThreeNewNet/trainEcgAutoEnc.py
@@ -20,75 +20,37 @@ import glob, os
 import struct
 
 from anomolyDetector import AnomalyDetector
-
-autoencoder = AnomalyDetector()
-
-autoencoder.compile(optimizer='adam', loss='mae')
+from anomolyDetector import DataGenerator
 
 # -------------------------------------
 
-lidar_data = np.array([])
+autoencoder = AnomalyDetector()
+autoencoder.compile(optimizer='adam', loss='mae')
 
 path = "/Users/garrettchristian/DocumentsDesktop/uva21/summerProject/lidarTests/data/sets/kitti/dataset/sequences/00/"
-for file in glob.glob(path + "**/000*.bin", recursive = True):
-    print(file)
-    pcd = np.fromfile(file, dtype=np.float32)
-    # print(np.shape(pcd))
-    
-    pcd = pcd.reshape((int(np.shape(pcd)[0]) // 4, 4))
-    pcd = np.delete(pcd, 3, 1)
-    pcd = pcd.reshape(np.size(pcd),)
-    pcd = np.pad(pcd, (0,1552704 - int(np.shape(pcd)[0])), 'constant', constant_values=(0))
-    
-    # print(np.shape(pcd))
-    # print(pcd)
+print(glob.glob(path + "**/000*.bin", recursive = True))
 
-    if (np.size(lidar_data)):
-        lidar_data = np.vstack([lidar_data, pcd])
-    else:
-        lidar_data = np.array([pcd])
+files = np.array(glob.glob(path + "**/00*.bin", recursive = True))
+print(np.shape(files))
 
-print(lidar_data)
-print(np.shape(lidar_data))
+# Parameters
+params = {'dim': (2070272,),
+          'batch_size': 100,
+          'n_channels': 1,
+          'shuffle': True}
 
-labels = np.ones(np.shape(lidar_data)[0])
-# print(labels)
-# print(np.size(labels))
+
+# Datasets
+labels = np.ones(np.shape(files)[0]) # Labels
 
 train_data, test_data, train_labels, test_labels = train_test_split(
-    lidar_data, labels, test_size=0.2, random_state=21
+    files, labels, test_size=0.2, random_state=21
 )
 
+# Generators
+training_generator = DataGenerator(train_data, train_labels, **params)
+validation_generator = DataGenerator(test_data, test_labels, **params)
 
-# min_val = tf.reduce_min(train_data)
-# max_val = tf.reduce_max(train_data)
-
-# train_data = (train_data - min_val) / (max_val - min_val)
-# test_data = (test_data - min_val) / (max_val - min_val)
-
-# train_data = tf.cast(train_data, tf.float32)
-# test_data = tf.cast(test_data, tf.float32)
-
-# # You will train the autoencoder using only the normal rhythms, which are labeled in this dataset as 1. 
-# # Separate the normal rhythms from the abnormal rhythms.
-
-# train_labels = train_labels.astype(bool)
-# test_labels = test_labels.astype(bool)
-
-# normal_train_data = train_data[train_labels]
-# normal_test_data = test_data[test_labels]
-
-# anomalous_train_data = train_data[~train_labels]
-# anomalous_test_data = test_data[~test_labels]
-
-# Notice that the autoencoder is trained using only the normal ECGs, 
-# but is evaluated using the full test set.
-
-history = autoencoder.fit(train_data, train_data, 
-          epochs=20, 
-          batch_size=512,
-          validation_data=(test_data, test_data),
-          shuffle=True)
-
+history = autoencoder.fit(training_generator, validation_data=validation_generator, epochs=1)
 
 autoencoder.save("pcdModel")
